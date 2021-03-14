ac6807227f19517aa2fd2c54dc7330bd50309332
oksanabolbat <oksana.bolbat@gmail.com>
2021-03-14 20:11:31 +0200

    init: 2 strange errors in the transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,27 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error');
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  //throw new CustomError('Not implemented');
+  //remove line with error and write your code here
+  if (!Array.isArray(arr)) throw new CustomError("It isn't an array");
+  if (arr.length === 0) return [];
+  let trArr = [];
+  let commands = ['--discard-next', '--discard-prev', '--double-next', '--double-prev'];
+  //console.log('array ', arr);
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === commands[0]) {
+      i++;
+    } else if (arr[i] === commands[1] && i > 0) {
+      trArr.pop();
+    } else if (arr[i] === commands[2] && i < arr.length - 1) {
+      trArr.push(arr[i + 1]);
+    } else if (arr[i] === commands[3] && i > 0) {
+      trArr.push(arr[i - 1]);
+    } else {
+      if (!commands.includes(arr[i])) trArr.push(arr[i]);
+    }
+  }
+  if (arr[3] ===1337) {console.log('array ', arr, 'tr array ', trArr);}
+  //
+  return trArr;
 };
